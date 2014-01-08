7939a06ccf2b86ee11a625a1d8762b796c32b631
Wojciech Żywno <w.zywno@gmail.com>
2014-01-08 13:12:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nuedu::Application.config.secret_key_base = '5d8359c019f74c1065f3b3a541b3ae8d58b8132d25d4802951836e40b996ee9891aed1132f56722894d42bb1a83a28436589d17ded6408fe3c700527d8f9af69'
