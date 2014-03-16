c7a023e3390da67bd65b5e426aef2a322997c33a
Wojciech Żywno <w.zywno@gmail.com>
2014-03-16 16:07:59 +0100

    elastic search fix

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,8 @@
+# Print Curl-formatted traces in development
+#
+if Rails.env.development?
+  tracer = ActiveSupport::Logger.new(STDERR)
+  tracer.level =  Logger::INFO
+end
+
+Elasticsearch::Model.client = Elasticsearch::Client.new( tracer: tracer, host: ENV['ELASTICSEARCH_URL'])
