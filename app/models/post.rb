98702dd401ada1c7ae9b7f0bc0a0876ca9b44e84
Wojciech Żywno <w.zywno@gmail.com>
2014-07-28 18:36:21 +0200

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  belongs_to :topic, :counter_cache => true, :touch => true 
-  
-  
+  belongs_to :topic, :counter_cache => true, :touch => true
+
+
   # Validations
   validates :body, :presence => true
   validates :user, :presence => true
-  
+
   # Default Scope
-  default_scope :order => 'created_at ASC'
-  
+  default_scope { where(order: 'created_at ASC') }
+
   # Scope to display only the last n posts. Used for "Recent Posts" display
   scope :recent, lambda {
     |c| reorder('created_at desc').limit(c)
   }
-  
+
 end
